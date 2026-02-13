bb2d7b6b171630f3d11f1335942eab2145f458fe
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2026-02-13 17:52:44 +0500

    created route for create event type

--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { createEventType } = require('../controllers/eventsController');
 
-router.post('/add-event-type', registerUser);
+router.post('/add-event-type', createEventType);
 
 module.exports = router;
