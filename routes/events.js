8b6a378818ac8ad1908a621cef37a50ba2da2dc5
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2026-02-14 14:22:13 +0500

    added route for create event controller

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createEventType, getEventTypes } = require('../controllers/eventsController');
+const { createEventType, getEventTypes, createEvent } = require('../controllers/eventsController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/add-event-type', protect, createEventType);
 router.get('/get-event-types', protect, getEventTypes);
+router.post('/create-event', protect, createEvent);
 
 module.exports = router;
