aab545d8eefa87f233742117525e57505bd6a170
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2026-02-17 11:08:50 +0500

    added delete-event route

--- routes/events.js
+++ routes/events.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createEventType, getEventTypes, createEvent, getEvents } = require('../controllers/eventsController');
+const { createEventType, getEventTypes, createEvent, getEvents, deleteEvent } = require('../controllers/eventsController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/add-event-type', protect, createEventType);
 router.get('/get-event-types', protect, getEventTypes);
 router.post('/create-event', protect, createEvent);
 router.get('/get-events', protect, getEvents);
+router.delete('/delete-event/:uid', protect, deleteEvent);
 
 module.exports = router;
